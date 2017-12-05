4cf3f4d7623fbd0390c66b348846eaf16f9afd9f
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-12-05 11:40:59 +0000

    Basic Splines!

---
+++ Assets/Editor/LineInspector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor {
+    private Vector3[] nodes;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnSceneGUI()
+    {
+        Line line = target as Line;
+        nodes = line.nodes;
+
+        Transform handleTransform = line.transform;
+        Quaternion handleRotation = handleTransform.rotation;
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            nodes[i] = handleTransform.TransformPoint(line.nodes[i]);
+        }
+        //Vector3 p0 = handleTransform.TransformPoint(line.p0);
+       // Vector3 p1 = handleTransform.TransformPoint(line.p1);
+
+        Handles.color = Color.blue;
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawLine(nodes[i], nodes[i+1]);
+            Handles.DoPositionHandle(nodes[i], handleRotation);
+            
+        }
+        
+    }
+}
