259028f3420161144aa332212bf6a1fb4cfee7bb
Ellie Richards <eleanorichards@hotmail.co.uk>
2017-12-05 14:29:01 +0000

    Here

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -7,12 +7,14 @@ public class BezierCurve : MonoBehaviour {
 
     public Vector3 GetPoint(float t)
     {
-        Vector3 transformPoint = Vector3.zero;
-        for(int i = 0; i < nodes.Length; i += 3)
-        {
-            transformPoint =  transform.TransformPoint(Bezier.GetPoint(nodes[i], nodes[i+1], nodes[i+2], t));
-        }
-        return transformPoint;
+        return transform.TransformPoint(Bezier.GetPoint(nodes[0], nodes[1], nodes[2], t));
+        //Vector3 transformPoint = Vector3.zero;
+        //for (int i = 3; i < nodes.Length; i++)
+        //{
+        //    transformPoint = transform.TransformPoint(Bezier.GetPoint(nodes[i], nodes[i - 1], nodes[i - 2], t));
+        //    i += 2;
+        //}
+        //return transformPoint;
     }
 
     public void Reset()
