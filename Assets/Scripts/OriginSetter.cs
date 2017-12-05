0e7cdc2cf055a7a134e4a0cd90ed269e7e9a4c3e
Ellie Richards <eleanor4.richards@live.uwe.ac.uk>
2017-12-05 09:39:31 +0000

    spline try

---
+++ Assets/Scripts/OriginSetter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OriginSetter : MonoBehaviour {
+
+    private List<GameObject> branch_origins = new List<GameObject>(100);
+
+    public GameObject generator;
+
+    public float x_pos;
+    public float y_pos;
+    public float z_pos;
+
+    public float x_rot;
+    public float y_rot;
+    public float z_rot;
+
+    public float num_of_branches = 10;
+
+	// Use this for initialization
+	void Start () {
+          SetBranchPositions();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetBranchPositions()
+    {
+        branch_origins.Clear();
+        for (int i = 0; i < num_of_branches; i++)
+        {
+            float angle = i * Mathf.PI * 2 / num_of_branches;
+            Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * 5;
+            Instantiate(generator, pos, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/splineGeometry.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class splineGeometry : MonoBehaviour {
+   
+	// Use this for initialization
+	void Start () {
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh mesh = mf.mesh;
+        //vertex locations
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(1,0,1),
+            new Vector3(-1,0,1),
+            new Vector3(1,0,-1),
+            new Vector3(-1,0,-1)
+        };
+        Vector3[] normals = new Vector3[]
+        {
+            new Vector3(0,1,0),
+            new Vector3(0,1,0),
+            new Vector3(0,1,0),
+            new Vector3(0,1,0)
+        };
+        //for texturing
+        Vector2[] uvs = new Vector2[]
+        {
+            new Vector2(0,1),
+            new Vector2(0,0),
+            new Vector2(1,1),
+            new Vector2(1,0)
+        };
+        //polygons
+        int[] triangles = new int[]
+        {
+            0,2,3,
+            3,1,0
+        };
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.normals = normals;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
